7904ed74063297c57c208e45b03814620d13a289
Andrew Dimes <andrewdimes@gmail.com>
2021-03-18 12:55:16 -0400

    update carousel and change font

--- public/js/main.js
+++ public/js/main.js
@@ -15,9 +15,15 @@ const options = {
     autoStart: true
 
 }
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.timepicker');
+  var instances = M.Timepicker.init(elems, options);
+});
 
-
-
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.datepicker');
+  var instances = M.Datepicker.init(elems, options);
+});
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
