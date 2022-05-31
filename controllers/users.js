509d83d9025a7a162d98991666c0956175964d2d
Andrew Dimes <andrewdimes@gmail.com>
2022-05-31 10:42:22 -0700

    Initial commit

--- controllers/users.js
+++ controllers/users.js
@@ -1,83 +1,10 @@
 const User = require('../models/user')
-const Cart = require('../models/cart')
 
 module.exports = {
-    index,
-    about,
-    menu,
-    order,
-    cart,
-    delete: deleteCart,
-    checkOut,
-    goCheckOut,
-    uhoh,
-    hhmenu,
-    drinks,
-    remove
-    
+    index  
 }
 
-function remove(req, res){
-    res.redirect('/cart')
-}
-
-function hhmenu(req, res){
-    res.render('hhmenu')
-}
-
-function drinks(req,res){
-    res.render('drinks')
-}
-
-function goCheckOut(req,res){
-    res.render('checkout')
-}
-
-function checkOut(req,res){
-    Cart.findByIdAndDelete(req.params.id,function(err){
-        delete User.cart
-        res.redirect('/checkout')
-    })   
-}
 
 function index(req, res, next){
     res.render('index')
-}
-function deleteCart(req, res) {
-    Cart.findByIdAndDelete(req.params.id,function(err){
-        delete User.cart
-        res.redirect('/cart')
-    })       
-}
-
-function about(req,res){
-    res.render('about')
-}
-
-function uhoh(req, res){
-    res.render('uhoh')
-}
-
-function menu(req,res){
-    res.render('menu')
-}
-
-function order(req,res){
-    res.render('order')
-}
-
-function cart(req,res){
-    let items;
-    let prices;
-    let total;
-    let cartId;
-    if(User.cart){
-       items = User.cart.item
-       prices = User.cart.price
-       total = User.cart.total
-       cartId = User.cart._id
-        res.render('cart', {items, prices, total, cartId})
-    } else {
-        res.render('cart',{items, prices, total, cartId})
-    }  
 }
--- routes/users.js
+++ routes/users.js
@@ -3,17 +3,7 @@ const usersCtrl = require('../controllers/users');
 const user = require('../models/user');
 
 router.get('/users', usersCtrl.index);
-router.get('/about', usersCtrl.about);
-router.get('/uhoh', usersCtrl.uhoh)
-router.get('/menu', usersCtrl.menu)
-router.get('/hhmenu', usersCtrl.hhmenu);
-router.get('/drinks', usersCtrl.drinks)
-router.get('/order',isLoggedIn, usersCtrl.order)
-router.get('/cart',isLoggedIn, usersCtrl.cart)
-router.get('/checkout', usersCtrl.goCheckOut)
-router.delete('/cart/:id', usersCtrl.delete)
-router.delete('/cart/:id/checkout', usersCtrl.checkOut)
-router.delete('/cart/:id/remove', usersCtrl.remove)
+
 
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()) return next()
